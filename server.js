347fd1d7a9d702c36e8c3d129042d271ba3941a2
Svitlana Kryzhanivska <lana19333@gmail.com>
2019-06-04 22:08:41 +0300

    Version 8.0.0

--- server.js
+++ server.js
@@ -26,12 +26,12 @@ app.use('/api/tasks', require('./routes/api/tasks'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
