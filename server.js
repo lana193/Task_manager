c64606065a937653aa4c1770f6faf253f722c1e4
Svitlana Kryzhanivska <lana19333@gmail.com>
2019-06-04 13:39:34 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-const mongoose = require('mongoose');
 const express = require('express');
-const config = require('config');
+const mongoose = require('mongoose');
 const path = require('path');
+const config = require('config');
 
-const API_PORT = 3001;
 const app = express();
 
+// Bodyparser Middleware
 app.use(express.json());
 
-const router = express.Router();
+// DB Config
+const db = config.get('mongoURI');
+
+// Connect to Mongo
+mongoose
+  .connect(db, { 
+    useNewUrlParser: true,
+    useCreateIndex: true
+  }) // Adding new mongo url parser
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/tasks', require('./routes/api/tasks'));
@@ -16,35 +26,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-  
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-// DB Config
-const dbRoute = config.get('mongoURI');
+const port = process.env.PORT || 3001;
 
-// Connect to Mongo DB
-mongoose.connect(
-  dbRoute,
-  { useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false }
-);
-
-let db = mongoose.connection;
-
-db.once("open", () => console.log("Connected to the database"));
-
-// Check if connection with the database is successful
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-// Append /api for http requests
-app.use("/api", router);
-
-// Launch backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
